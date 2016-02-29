927a40459cca9bf7782fcc85eff28e77484a98e1
natetc95 <natetc95@email.arizona.edu>
2016-02-29 01:38:54 -0700

    Initial Button Testing
    Writes value from ADC buffer of AN0 to the display. Useful to determine
    the ADC values for each row of the button pad. We will determine bounds
    within a 5% tolerance of voltage reported.

--- main.c
+++ main.c
@@ -15,14 +15,26 @@
 #include "keypad.h"
 #include "interrupt.h"
 
+volatile unsigned int val = 0;
+
 int main(void) {
     initTimer(1,0,4);
     initLCD();
     
     initKeypad();
     
+    char s[32];
+    
     while(1) {
-        
-    } 
+        itoa(val, s, 10);
+        printStringLCD(s);
+    }
+    
     return(0);
+}
+
+void __ISR(_ADC_VECTOR, IPL7AUTO) _ADCInterrupt(void) {
+    IFS0bits.AD1IF = 0;
+    AD1CHSbits.CH0SA = 0;
+    val = ADC1BUF0;
 }
