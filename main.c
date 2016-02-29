032bf62f55533dced8d560647eca92ed53ddc282
natetc95 <natetc95@email.arizona.edu>
2016-02-28 23:28:26 -0700

    Initial Commit

---
+++ main.c
@@ -0,0 +1,19 @@
+/********************************
+ * Ivan Cordoba
+ * Nathaniel Christianson
+ * Alex Levine
+ * Jordan Jeffries
+ * 
+ * ECE 372A
+ * LAB 2 PART 1
+ *******************************/
+
+#include <xc.h>
+#include <sys/attribs.h>
+#include "config.h"
+#include "lcd.h"
+#include "keypad.h"
+
+int main(void) {
+    
+}
